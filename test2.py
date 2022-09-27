237a10f583413b50cd69d55e7a26f79579024ea7
Liang Po Hsiang <bob132231@gmail.com>
2022-09-27 03:37:36 +0000

    GitHub上傳

--- test2.py
+++ test2.py
@@ -2,3 +2,9 @@ class addit(a,b)
   a = 1
   b = 2
   return a + b
+
+# git remote -v
+# git remote add origin {github_ip}
+# git remote -v
+# git push origin master
+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
